1e71b2e8d92c4622cfafcb1d77d21b26e94b96e9
zigac9 <62549216+zigac9@users.noreply.github.com>
2022-12-07 21:46:23 +0100

    Creating style for page,chat and users. Adding overflow to chat

--- static/js/functionality.js
+++ static/js/functionality.js
@@ -1,9 +1,20 @@
 function updateUserList(){
     let peers = context.users
+    let br = document.createElement("br");
     let list = document.querySelector("#tempUserList")
     list.innerHTML = ""
+    let myself = false;
     for (const [key, value] of Object.entries(peers)) {
-        list.innerHTML += value["username"] + " "
+        if (!myself)
+        {
+            list.innerHTML += "Jaz: " + value["username"] + " "
+        }
+        else
+        {
+            list.innerHTML += value["username"] + " ";
+        }
+        myself = true;
+        list.appendChild(br);
     }
 }
 
