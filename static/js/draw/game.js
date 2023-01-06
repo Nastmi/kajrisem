f0144924183f878be2b819431c5e263b1d876f16
zigac9 <62549216+zigac9@users.noreply.github.com>
2023-01-06 22:03:51 +0100

    TODO next round

--- static/js/draw/game.js
+++ static/js/draw/game.js
@@ -21,11 +21,14 @@ function nextRound() {
         type: "clearCanvas",
         selectColor: selectedColor
     }
+    //TODO poglej zakaj se nextRound tolikokrat kliče še preden kaj naredimo - vidiš v chatu!!
     const chat = document.querySelector("#chat");
     let br = document.createElement("br");
     let text = document.createTextNode("------NEXT ROUND------");
     chat.appendChild(text);
     chat.appendChild(br);
+    //END
+
     clearCanvass(data);
     fetch("/game/next-round", {
         method: "POST",
