0c6d4f6185d374816bf33140b84a07bacb22b10e
Nastmi <48620509+Nastmi@users.noreply.github.com>
2023-01-07 15:32:12 +0100

    Username works if connecting to the room as well.

--- static/js/index.js
+++ static/js/index.js
@@ -2,13 +2,13 @@ window.addEventListener("load", e => {
     document.querySelector("#room-btn").addEventListener("click", e => {
         let user = document.querySelector("#inputName").value
         url = "/room" + "?username=" + user
-        console.log(url)
         window.location.href = url
     })
 
     document.querySelector("#play-btn").addEventListener("click", e => {
         let roomNum = document.querySelector("#inputRoom").value
-        url = "/room/" + roomNum
+        let user = document.querySelector("#inputName").value
+        url = "/room/" + roomNum + "?username="+ user
         window.location.href = url
     })
 })
